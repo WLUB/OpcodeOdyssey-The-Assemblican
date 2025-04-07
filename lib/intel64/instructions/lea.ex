9b641f8362fe8a1fb759864d264f0dba44c32054
Lukas Bergström <lukas.bergstrom99@gmail.com>
2025-04-07 17:57:48 +0200

    Extended register support

--- lib/intel64/instructions/lea.ex
+++ lib/intel64/instructions/lea.ex
@@ -4,9 +4,13 @@ defmodule Assembler.Intel64.Instruction.LEA do
   """
   use Assembler.Intel64.Instruction,
   construct: fn
-    %LEA {parameters: [%Register{reg: reg, size: 64}, mem_operand]} ->
+    %LEA {parameters: [%Register{reg: reg, size: 64}, mem_operand]} when reg >= 0 and reg < 8 ->
       modrm = (reg <<< 3) ||| 5
       <<0x48, 0x8D, modrm, mem_operand :: little-unsigned-integer-size(32)>>
+
+    %LEA {parameters: [%Register{reg: reg, size: 64}, mem_operand]} when reg >= 8 and reg < 16 ->
+      modrm = ((reg - 8) <<< 3) ||| 5
+      <<0x4C, 0x8D, modrm, mem_operand :: little-unsigned-integer-size(32)>>
   end,
   deconstruct: fn
     nil -> nil
